6ff9de2b058db349d0fba9621efc2035b3f7c53f
Michael Phillips <github@idiotandrobot.com>
2014-10-07 19:51:19 +0100

    Add progress output to console

--- Program.cs
+++ Program.cs
@@ -12,7 +12,7 @@ namespace ReadMarkdownParser
     {
         static void Main(string[] args)
         {
-
+            int fileCount = 0;
             try
             {
 
@@ -28,38 +28,50 @@ namespace ReadMarkdownParser
                 {
                     for (int i = 0; i < match.Groups["Read"].Captures.Count; i++)
                     {
-                        var read = new Read();
-                        read.Title = match.Groups["Title"].Captures[i].Value;
-                        read.Author = match.Groups["Author"].Captures[i].Value;
-                        read.Finished = DateTime.Parse(match.Groups["Year"].Value
-                            + "-"
-                            + match.Groups["Month"].Captures[i].Value
-                            + "-"
-                            + match.Groups["Day"].Captures[i].Value);
+                        try
+                        {
+                            var read = new Read();
+                            read.Title = match.Groups["Title"].Captures[i].Value;
+                            read.Author = match.Groups["Author"].Captures[i].Value;
+                            read.Finished = DateTime.Parse(match.Groups["Year"].Value
+                                + "-"
+                                + match.Groups["Month"].Captures[i].Value
+                                + "-"
+                                + match.Groups["Day"].Captures[i].Value);
 
-                        string filePath = 
-                            string.Format("{0}-{1}.md",
-                            read.Finished.ToString("yyyy-MM-dd"),
-                            read.Title
-                                .Replace(" - ", "-")
-                                .Replace(":", "")
-                                .Replace(" ", "-")                                
-                            );
+                            string filePath =
+                                string.Format("{0}-{1}.md",
+                                read.Finished.ToString("yyyy-MM-dd"),
+                                read.Title
+                                    .Replace(" - ", "-")
+                                    .Replace(":", "")
+                                    .Replace(" ", "-")
+                                );
 
-                        string fileContents = 
-                            string.Format("---\nlayout: post\ntitle: {0}\nsubtitle: {1}\ndate: {2}\n---\n",
-                            read.Title,
-                            read.Author,
-                            read.Finished.ToString("yyyy-MM-dd"));
+                            string fileContents =
+                                string.Format("---\nlayout: post\ntitle: {0}\nsubtitle: {1}\n---\n",
+                                read.Title,
+                                read.Author);
 
-                        File.WriteAllText(Path.Combine(postDir, filePath), fileContents);
+                            File.WriteAllText(Path.Combine(postDir, filePath), fileContents);
+                            Console.WriteLine(filePath);
+                            fileCount += 1;
+                        }
+                        catch (Exception)
+                        {
+                            throw;
+                        }
                     }
                 }
+                Console.WriteLine("{0} files created", fileCount);
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
+            
+            Console.WriteLine("");
+            Console.Write("Press any key...");
             Console.ReadKey();
         }
     }
